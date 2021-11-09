470cb35416466b9d2d05659617f02b2c4a4237ba
krns21 <krns.0021@gmail.com>
2021-11-09 23:05:52 +0000

    updated to v13

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - [ ] Music
 - [ ] Voice Channel Control
 - [ ] Greeting
+
+## Dependencies:
+- discord.js @ 12.5.3
+- ffmpeg @ 0.0.4
+- ytdl-core @ 4.9.1
--- bot.js
+++ bot.js
@@ -1,28 +1,25 @@
 // Run dotenv
 require('dotenv').config();
 
-//Import Libraries
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// Initialize bot by connecting to the server
-client.login(process.env.DISCORD_TOKEN);
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-// Event listener when a user connected to the server
+
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
 });
 
 
-client.on('message', msg => {
-    if (!msg.content.startsWith('!')) return;
-    if (msg.content.startsWith('!play')) {
-        msg.reply('Playing')
-    } else if (msg.content.startsWith('!skip')) {
-        msg.reply('Skipped')
-    } else if (msg.content.startsWith('!stop')) {
-        msg.reply('stopped')
-    } else {
-        msg.reply('Invalid Command')
-    }
-})
+client.login(process.env.DISCORD_TOKEN);
