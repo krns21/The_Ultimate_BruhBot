4c0072551f47a9f830b58773eafb54a048b43347
krns21 <krns.0021@gmail.com>
2021-11-09 23:48:45 +0000

    clean build

--- bot.js
+++ bot.js
@@ -1,25 +1,19 @@
 // Run dotenv
 require('dotenv').config();
 
+// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
+
+// Create bot token const
+const token = process.env.DISCORD_TOKEN;
+
+// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
-
-client.login(process.env.DISCORD_TOKEN);
+// Login to Discord
+client.login(token);
