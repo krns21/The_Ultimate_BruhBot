095f93af0b83b2bcaea78c1c49e4d8accdbdf565
krns21 <krns.0021@gmail.com>
2021-11-14 23:37:46 +0000

    Added event handling and skip

---
+++ events/interactionCreate.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        console.log(`${interaction.user.tag} triggered an interaction.`);
+        // Returns nothing if interaction is not part of commands
+        if (!interaction.isCommand()) return;
+
+        const { client } = require('..');
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true });
+        }
+    },
+};
